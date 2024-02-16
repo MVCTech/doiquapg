ecd0297ef0665f2f5bf9f04383635aef4f49ac50
taipham2000 <taitinpro@gmail.com>
2024-02-16 17:06:57 +0700

    FreeFire

--- src/pages/SpinFreeFire/SpinFreeFire.js
+++ src/pages/SpinFreeFire/SpinFreeFire.js
@@ -9,27 +9,32 @@ import "./SpinFreeFire.css";
 import { useEffect, useState } from "react";
 
 export default function SpinFreeFire() {
-  const [current, setCurrent] = useState(0);
-  const [iteration, setIteration] = useState(1);
-  const [maxCount, setMaxCount] = useState(8);
+  const [count, setCount] = useState(0);
+  const [isLasted, setIsLasted] = useState(false);
+  const [countRound, setCountRound] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (current < maxCount) {
-        console.log("test: ", current);
-        setCurrent((prevCount) => prevCount + 1);
-      } else {
-        if (iteration < 8) {
-          console.log("test iter: ", current);
-          setIteration((prevIteration) => prevIteration + 1);
-          setCurrent(0);
-          setMaxCount(8); // Set maxCount to 5 for the last iteration
+      if (count < 8 && !isLasted) {
+        if (count === 7) {
+          setCountRound((prevCount) => prevCount + 1);
+          setCount(0);
+          if (countRound === 4) {
+            setIsLasted(true);
+          }
+        } else {
+          setCount((prevCount) => prevCount + 1);
+        }
+      } else if (isLasted) {
+        if (count < 7) {
+          setCount((prevCount) => prevCount + 1);
+        }
+        if (count === 7) {
+          return () => clearTimeout(timer);
         }
       }
-    }, 100); // 1000 milliseconds = 1 second
-
-    return () => clearTimeout(timer); // Cleanup function to clear the timer
-  }, [current, iteration, maxCount]); // Re-run effect when count, iteration, or maxCount changes
+    }, 80);
+  }, [count, isLasted, countRound]);
 
   return (
     <div className="relative top-0">
@@ -43,14 +48,11 @@ export default function SpinFreeFire() {
             <div className="relative top-0">
               <div className="relative z-30 top-[6%] scale-spin">
                 <div className="relative z-30">
-                  <img
-                    src={current === 0 ? PIECE_YELLOW : PIECE}
-                    className=""
-                  />
+                  <img src={count === 0 ? PIECE_YELLOW : PIECE} className="" />
                   <div className="absolute -top-8 z-40 w-[90px] left-1/2 -translate-x-1/2">
                     <div className="relative">
                       <img
-                        src={current === 0 ? BG_PIECE_YELLOW : BG_PIECE}
+                        src={count === 0 ? BG_PIECE_YELLOW : BG_PIECE}
                         className=""
                       />
                       <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
@@ -73,13 +75,10 @@ export default function SpinFreeFire() {
                   </div>
                 </div> */}
                 <div className="absolute top-0 rotate-spin-1">
-                  <img
-                    src={current === 1 ? PIECE_YELLOW : PIECE}
-                    className=""
-                  />
+                  <img src={count === 1 ? PIECE_YELLOW : PIECE} className="" />
                   <div className="absolute -top-8 z-40 w-[90px] left-1/2 -translate-x-1/2">
                     <div className="relative">
-                      <img src={current === 1 ? BG_PIECE_YELLOW : BG_PIECE} />
+                      <img src={count === 1 ? BG_PIECE_YELLOW : BG_PIECE} />
                       <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
                         {" "}
                         hello
@@ -88,13 +87,10 @@ export default function SpinFreeFire() {
                   </div>
                 </div>
                 <div className="absolute top-0 rotate-spin-2">
-                  <img
-                    src={current === 2 ? PIECE_YELLOW : PIECE}
-                    className=""
-                  />
+                  <img src={count === 2 ? PIECE_YELLOW : PIECE} className="" />
                   <div className="absolute -top-8 z-40 w-[90px] left-1/2 -translate-x-1/2">
                     <div className="relative">
-                      <img src={current === 2 ? BG_PIECE_YELLOW : BG_PIECE} />
+                      <img src={count === 2 ? BG_PIECE_YELLOW : BG_PIECE} />
                       <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
                         {" "}
                         hello
@@ -103,13 +99,10 @@ export default function SpinFreeFire() {
                   </div>
                 </div>
                 <div className="absolute top-0 rotate-spin-3">
-                  <img
-                    src={current === 3 ? PIECE_YELLOW : PIECE}
-                    className=""
-                  />
+                  <img src={count === 3 ? PIECE_YELLOW : PIECE} className="" />
                   <div className="absolute -top-8 z-40 w-[90px] left-1/2 -translate-x-1/2">
                     <div className="relative">
-                      <img src={current === 3 ? BG_PIECE_YELLOW : BG_PIECE} />
+                      <img src={count === 3 ? BG_PIECE_YELLOW : BG_PIECE} />
                       <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
                         {" "}
                         hello
@@ -118,13 +111,10 @@ export default function SpinFreeFire() {
                   </div>
                 </div>
                 <div className="absolute top-0 rotate-spin-4">
-                  <img
-                    src={current === 4 ? PIECE_YELLOW : PIECE}
-                    className=""
-                  />
+                  <img src={count === 4 ? PIECE_YELLOW : PIECE} className="" />
                   <div className="absolute -top-8 z-40 w-[90px] left-1/2 -translate-x-1/2">
                     <div className="relative">
-                      <img src={current === 4 ? BG_PIECE_YELLOW : BG_PIECE} />
+                      <img src={count === 4 ? BG_PIECE_YELLOW : BG_PIECE} />
                       <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
                         {" "}
                         hello
@@ -133,13 +123,10 @@ export default function SpinFreeFire() {
                   </div>
                 </div>
                 <div className="absolute top-0 rotate-spin-5">
-                  <img
-                    src={current === 5 ? PIECE_YELLOW : PIECE}
-                    className=""
-                  />
+                  <img src={count === 5 ? PIECE_YELLOW : PIECE} className="" />
                   <div className="absolute -top-8 z-40 w-[90px] left-1/2 -translate-x-1/2">
                     <div className="relative">
-                      <img src={current === 5 ? BG_PIECE_YELLOW : BG_PIECE} />
+                      <img src={count === 5 ? BG_PIECE_YELLOW : BG_PIECE} />
                       <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
                         {" "}
                         hello
@@ -148,13 +135,10 @@ export default function SpinFreeFire() {
                   </div>
                 </div>
                 <div className="absolute top-0 rotate-spin-6">
-                  <img
-                    src={current === 6 ? PIECE_YELLOW : PIECE}
-                    className=""
-                  />
+                  <img src={count === 6 ? PIECE_YELLOW : PIECE} className="" />
                   <div className="absolute -top-8 z-40 w-[90px] left-1/2 -translate-x-1/2">
                     <div className="relative">
-                      <img src={current === 6 ? BG_PIECE_YELLOW : BG_PIECE} />
+                      <img src={count === 6 ? BG_PIECE_YELLOW : BG_PIECE} />
                       <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
                         {" "}
                         hello
@@ -163,13 +147,10 @@ export default function SpinFreeFire() {
                   </div>
                 </div>
                 <div className="absolute top-0 rotate-spin-7">
-                  <img
-                    src={current === 7 ? PIECE_YELLOW : PIECE}
-                    className=""
-                  />
+                  <img src={count === 7 ? PIECE_YELLOW : PIECE} className="" />
                   <div className="absolute -top-8 z-40 w-[90px] left-1/2 -translate-x-1/2">
                     <div className="relative">
-                      <img src={current === 7 ? BG_PIECE_YELLOW : BG_PIECE} />
+                      <img src={count === 7 ? BG_PIECE_YELLOW : BG_PIECE} />
                       <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
                         {" "}
                         hello
